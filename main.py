d48357dd621fcbba49b1cebe950f423c2468ef2d
mh53182 <marie.i.077145@gmail.com>
2024-01-26 16:17:20 +0900

    引数の扱い

--- main.py
+++ main.py
@@ -1,21 +1,60 @@
-def main():
-    leap_year_check()
+# 関数まわりメモ
 
-def leap_year_check():
-    T = int(input())
-    N = int(input())
+# 52. 位置引数のタプル化
 
-    if N % 400 == 0:
-        print(N, "is a leap year")
+# 引数の前にアスタリスク"*"をつけると、渡されてくる引数の数が分からなくても全部タプルに入れてくれる
+def say_something(word, *args):
+    print('word =', word)
+    for arg in args:
+        print(arg)
 
-    elif N % 4 == 0 and N % 100 != 0:
-        print(N, "is a leap year")
+say_something('Hi!', 'Mike', 'Nancy')
 
-    else:
-        print(N, "is not leap year")
 
-if __name__ == "__main__":
-    main()
+print('##################################')
+
+# 53. キーワード引数の辞書化
+
+# 引数の前にアスタリスク2つ"**"をつけると、渡されてくるキーワード引数を全部辞書に入れてくれる
+def menu(**kwargs): # keyword arguments
+    # print(kwargs)
+    for k, v in kwargs.items():
+        print(k, v)
+
+# menu('entree': 'beef', 'drink': 'coffee',)
+
+d = {
+    'entree': 'beef',
+    'drink': 'coffee',
+    'dessert': 'ice'
+}
+
+menu(**d)
+
+
+
+print('##################################')
+
+# 以下、閏年判定
+
+# def main():
+#     leap_year_check()
+
+# def leap_year_check():
+#     T = int(input())
+#     N = int(input())
+
+#     if N % 400 == 0:
+#         print(N, "is a leap year")
+
+#     elif N % 4 == 0 and N % 100 != 0:
+#         print(N, "is a leap year")
+
+#     else:
+#         print(N, "is not leap year")
+
+# if __name__ == "__main__":
+#     main()
 
 # 西暦が4で割り切れる年は閏年。
 # ただし、100で割り切れる年は閏年ではない。
