4f916df598c85c2ce725ce5bd0dee298089b5e7a
mh53182 <marie.i.077145@gmail.com>
2024-01-29 17:05:42 +0900

    ラムダ

--- lesson.py
+++ lesson.py
@@ -4,3 +4,14 @@ name = '1'
 new_num = int(name)
 
 print(new_num, type(new_num))
+
+print('########################')
+
+# return文演習
+# https://aiacademy.jp/texts/show/?id=572
+
+def gen_text(text, num):
+    return text * num
+
+text = gen_text("繰り返したい文字", 3)
+print(text)
