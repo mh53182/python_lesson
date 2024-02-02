97dbd795d27c4036aced2226c564bb00f18f04e2
mh53182 <marie.i.077145@gmail.com>
2024-02-02 10:16:35 +0900

    独自例外

--- error_handling.py
+++ error_handling.py
@@ -23,3 +23,30 @@ else:
 # 必ず実行される
 finally:
     print('clean up')
+
+
+print('##########################')
+
+# 66. 独自例外の作成
+
+# Exception を継承して自作のエラー(ここでは'UppercaseError')を作る
+class UppercaseError(Exception):
+    pass
+
+# raise文で明示的にエラーを起こす
+def check():
+    words = ['APPLE', 'orange', 'banana']
+    for word in words:
+        if word.isupper():
+            raise UppercaseError(word)
+        
+# 直接checkファンクションを実行すると"UppercaseError: APPLE"が出力される
+# check()
+
+# try exceptを使って、自作エラーに引っかかった際の処理を行う
+try:
+    check()
+except UppercaseError as exc:
+    print('This is my fault. Go next')
+# ※独自エラーにPython本来のエラー名を使わない方がベター
+# Pythonのエラーが出ているように見えると、その対応が必要だと思われるため
